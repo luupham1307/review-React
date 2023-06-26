5782dcdc33621fcd265f37d48f1c3b394936275f
luupham1307 <hoangluupham1307@gmail.com>
2023-06-27 00:21:53 +0700

    bt4

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,10 +1,20 @@
-function ExpenseItem() {
+import './ExpenseItem.css';
+
+function ExpenseItem(props) {
+    const month = props.date.toLocaleString('en-US', { month: 'long' });
+    const day = props.date.toLocaleString('en-US', { day: '2-digit' });
+    const year = props.date.getFullYear();
+
     return (
-        <div>
-            <div>March 28th 2021</div>
+        <div className='expense-item'>
             <div>
-                <h2>Car Insurance</h2>
-                <div>$294.67</div>
+                <div>{month}</div>
+                <div>{year}</div>
+                <div>{day}</div>
+            </div>
+            <div className='expense-item__description'>
+                <h2>{props.title}</h2>
+                <div className='expense-item__price'>${props.amount}</div>
             </div>
         </div>
     );
